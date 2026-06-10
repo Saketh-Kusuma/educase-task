43f05c8bff2e4c84adba8c754c7fb7df5caf4a58
Saketh-Kusuma <kusumasaketh92@gmail.com>
2026-06-11 00:57:17 +0530

    Remove border from Layout component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col primary-bg mx-auto w-[375px] h-[812px] border-1 border-black">
+    <div className="flex flex-col primary-bg mx-auto w-[375px] h-[812px]">
         <Outlet/>
     </div>
   );

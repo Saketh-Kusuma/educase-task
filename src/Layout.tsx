92da19c881d8566f8bfff8bd72eb2f0feb7c0883
Saketh-Kusuma <kusumasaketh92@gmail.com>
2026-06-11 09:53:02 +0530

    Refactor Layout component for improved styling consistency

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col primary-bg mx-auto w-[375px] h-[812px]">
-        <Outlet/>
+    <div className="flex flex-col primary-bg mx-auto w-[375px] h-[812px] flex-shrink-0 flex-grow-0 overflow-hidden box-border">
+      <Outlet />
     </div>
   );
-}
-
+};
 export default Layout
